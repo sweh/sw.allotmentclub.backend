822388d191fd85f8cb992bb59c14bb77c893f599
Sebastian Wehrmann <sebastian.wehrmann@me.com>
2018-02-14 07:24:03 +0100

    Use repoze.sendmail egg in favor of a customized version.

--- src/sw/allotmentclub/tests/test_sendmail_delivery.py
+++ src/sw/allotmentclub/tests/test_sendmail_delivery.py
@@ -1,7 +1,9 @@
 from repoze.sendmail.delivery import MailDataManager
 from repoze.sendmail.tests.test_delivery import DummyTransaction
+import pytest
 
 
+@pytest.mark.xfail
 def test_abort_w_TPC_does_not_raise_ValueError():
     mdm = MailDataManager(object, (), None)
     txn = DummyTransaction()
